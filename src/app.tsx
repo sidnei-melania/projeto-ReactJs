299e7dc347c97ee8cf14527ed37995fd2b8d36a8
sidnei-melania <164648960+sidnei-melania@users.noreply.github.com>
2024-12-31 02:03:27 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -101,23 +101,23 @@ export function App() {
           </div>
 
           {isGuestsInputOpen && (
-            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
-              <UserRoundPlus className="size-5 text-zinc-400" />
-              <span className="text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
+            <div className = "h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <button type = "button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
+              <UserRoundPlus className = "size-5 text-zinc-400" />
+              <span className = "text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
             </button>
 
             <div className="w-px h-6 bg-zinc-800" />
 
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button className = "bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               Confirmar viagem
-              <ArrowRight className="size-5" />
+              <ArrowRight className = "size-5" />
             </button>
           </div>
           )}
         </div>
 
-        <p className="text-sm text-zinc-500">
+        <p className = "text-sm text-zinc-500">
           Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
           com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
         </p>
